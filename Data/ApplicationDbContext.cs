c8f5a3a4561daf9ae19ee4bb14d4b9ae0603b0ad
Delia-Gherasim <115105029+Delia-Gherasim@users.noreply.github.com>
2024-10-30 20:48:25 +0200

    crapa daca dau submit fara sa selectez la toate

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using ElderyPeopleCare.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ElderyPeopleCare.Data
@@ -9,5 +10,35 @@ namespace ElderyPeopleCare.Data
             : base(options)
         {
         }
+        public DbSet<Challange> Challanges { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<ChallangeQuestions> ChallangeQuestions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ChallangeQuestions>()
+                .HasKey(cq => new { cq.cId, cq.questionId });
+            modelBuilder.Entity<ChallangeQuestions>()
+                .HasOne(cq => cq.Challange)
+                .WithMany(c => c.ChallangeQuestions)
+                .HasForeignKey(cq => cq.cId)
+                .OnDelete(DeleteBehavior.Cascade);
+            modelBuilder.Entity<ChallangeQuestions>()
+                .HasOne(cq => cq.Question)
+                .WithMany(q => q.ChallangeQuestions)
+                .HasForeignKey(cq => cq.questionId)
+                .OnDelete(DeleteBehavior.Cascade);
+            modelBuilder.Entity<Question>()
+                .Property(q => q.qId)
+                .ValueGeneratedOnAdd();
+
+            modelBuilder.Entity<Challange>()
+                .Property(c => c.cId)
+                .ValueGeneratedOnAdd();
+        }
     }
+
 }
+
